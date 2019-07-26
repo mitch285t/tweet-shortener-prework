1d35540530ab337c4d403f1df6a5b46aa1725dac
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 12:39:08 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def dictionary 
+  my_hash = {"hello" => "hi", 
+  "to" => "2", 
+  "two" => "2", 
+  "too" => "2", 
+  "for" => "4", 
+  "four" => "4", 
+  "be" => "b", 
+  "you" => "u", 
+  "at" => "@", 
+  "and" => "&"}
+end
+
+def word_substituter (string)
+  array = string.split
+  array.each_with_index do |words,index|
+    dictionary.keys.each do |x|
+      if x == words 
+      array[index] = dictionary[x]
+      end
+      end
+      end
+array.join(" ")
+end
+     
+    
+    
